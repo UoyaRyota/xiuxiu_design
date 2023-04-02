86ec3937e3f8730fe2a59b8e7d80a1828eff5740
uoyaryota <lestwinsdance8@gmail.com>
2023-04-02 13:00:03 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is an exclusive WordPress theme for XIUXIU DESIGN.
+ * Basically, the content is created for the purpose of displaying your works and making inquiries
+ * We plan to add necessary functions as the version is upgraded.
+ *
+ * @package WordPress
+ * @subpackage Xiuxiu_Design
+ * @since xiuxiu design 1.0
+ */
